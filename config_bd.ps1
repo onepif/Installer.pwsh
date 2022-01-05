aa258808abf288021441ed992fb5f23e91a32332
Dmitriy L. Ivanov <onepif@hp240g8.nnz-ipc.io>
2022-01-06 02:03:26 +0300

    fix bug configDB

--- config_bd.ps1
+++ config_bd.ps1
@@ -1,3 +1,12 @@
+[CmdletBinding()]
+Param(
+	[Parameter(Mandatory = $true)]
+    [string]$sectors,
+
+	[Parameter(Mandatory = $true)]
+    [string]$runways
+)
+
 Set-Location "$($Data.PATH_PELENG)\configDB"
 
 $env:PATH += "C:\oraclexe\app\oracle\product\11.2.0\server\bin"
@@ -7,4 +16,4 @@ sqlplus.exe SYSTEM/password "@db_prepare.sql"
 imp.exe master/master file=master.dmp
 imp.exe aero/aero file=aero.dmp
 
-sqlplus.exe master/master "@db_update.sql" $Args
+sqlplus.exe master/master "@db_update.sql" "$sectors" $runways.split()
